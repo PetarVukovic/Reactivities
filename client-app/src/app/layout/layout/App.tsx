317f2e888b71002da31015487568066671673a25
Petar Vukovic <petar.vukovic@adnet.hr>
2023-01-10 13:32:17 +0100

    End of section 6

--- client-app/src/app/layout/layout/App.tsx
+++ client-app/src/app/layout/layout/App.tsx
@@ -1,10 +1,11 @@
 import React, {  useEffect, useState } from 'react';
-import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid}from 'uuid';
+import agent from '../../api/agent';
+import LoadingComponents from './LoadingComponents';
 
 
 function App() {
@@ -12,12 +13,18 @@ function App() {
   const [activities,setActivities]=useState<Activity[]>([]);
   const[selectedActivity,setSelectedActivity]=useState<Activity | undefined>(undefined);//nas activity moze biti activitiy ili undefined a inicijalno stanje mu je undef
   const[editMode,setEditMode]=useState(false);
-  //sto zelimo uciniti kad se nasa apliakcija loada a zelimo dobiti data from our API
-  //Nakon sto se effecdft izvrsi postavit ce data u activities i ponavljat ce se infinite puta zato je trebamo ogranicit
+  const [loading,setLoading]=useState(true);
+  const[submitting,setSubmmiting]=useState(false);
   useEffect(()=>{
-    axios.get<Activity[]>('http://localhost:5000/api/Activities')
+    agent.Activities.list()
     .then(response=>{
-      setActivities(response.data);
+      let activities:Activity[]=[];
+      response.forEach(activity=>{
+        activity.date=activity.date.split('T')[0];
+        activities.push(activity);
+      })
+      setActivities(activities);
+      setLoading(false);
     })
   },[])
 
@@ -36,17 +43,41 @@ function handleFormClose(){
 }
 
 function handleCreateOrEditActivity(activity:Activity){
-  //provjeravamo imamo li aktiviti ako da filtriramo array sa novim prosljedenim parametrom activitiy
-  activity.id? setActivities([...activities.filter(x=>x.id !==activity.id),activity])
-  :setActivities([...activities,{...activity,id:uuid()}]);
-  setEditMode(false);
-  setSelectedActivity(activity);
+  setSubmmiting(true);
+  if(activity.id){
+    agent.Activities.update(activity).then(()=>{
+      setActivities([...activities.filter(x=>x.id !==activity.id),activity])
+      setSelectedActivity(activity);
+      setEditMode(false);
+      setSubmmiting(false);
+
+    })
+  }else{
+    activity.id=uuid();
+    agent.Activities.create(activity).then(()=>{
+      setActivities([...activities,activity])
+      setSelectedActivity(activity);
+      setEditMode(false);
+      setSubmmiting(false);
+    })
+
+  }
+  
+ 
 }
 
 function handleDeleteActivity(id:string)
 {
+  setSubmmiting(true);
+  agent.Activities.delete(id).then(()=>{
+    setActivities([...activities.filter(x=>x.id!==id)]);
+    setSubmmiting(false);
+
+  })
+
   setActivities([...activities.filter(x=>x.id!==id)])
 }
+if(loading) return <LoadingComponents content='Loading app'/>
  
   return (
 
@@ -62,6 +93,7 @@ function handleDeleteActivity(id:string)
         closeForm={handleFormClose}
         createOrEdit={handleCreateOrEditActivity}
         deleteActivity={handleDeleteActivity}
+        submitting={submitting}
         />
         </Container>
  
